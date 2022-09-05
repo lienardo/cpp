326fa3fa574256311548f720fd43dc00d99f7fa0
Vincentius Lienardo <vincentius.lienardo@protonmail.com>
2022-09-05 18:53:23 +0700

    Create 1-two-sum.cpp

---
+++ amazon/arrays-and-strings/1-two-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
+            auto it = m.find(target - nums[i]);
+            if (it != m.end()) {
+                return {it->second, i};
+            }
+            m[nums[i]] = i;
+        }
+        return {};
+    }
+};
