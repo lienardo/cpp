c4e57c4074fa870855b78a109ddb502641facc29
Vincentius Lienardo <vincentius.lienardo@protonmail.com>
2022-09-08 22:30:05 +0700

    Update missing-number.cpp

--- cses/introductory-problems/missing-number.cpp
+++ cses/introductory-problems/missing-number.cpp
@@ -5,11 +5,11 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-    ll n, a, sum = 0;
+    ll n, el, sum = 0;
     cin >> n;
     for (int i = 0; i < n - 1; i++) {
-        cin >> a;
-        sum += a;
+        cin >> el;
+        sum += el;
     }
     cout << n * (n + 1) / 2 - sum << '\n';
     return 0;
