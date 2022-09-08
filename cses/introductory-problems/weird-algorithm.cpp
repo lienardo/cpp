f1e0f7495d5efc78f923d66c9b40de498dc5502a
Vincentius Lienardo <vincentius.lienardo@protonmail.com>
2022-09-08 20:27:02 +0700

    Create weird-algorithm.cpp

---
+++ cses/introductory-problems/weird-algorithm.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << n;
+    while (n > 1) {
+        if (n & 1) n = 3 * n + 1;
+        else n >>= 1;
+        cout << " " << n;
+    }
+    cout << '\n';
+    return 0;
+}
